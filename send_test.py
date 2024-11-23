2bf24902fe2eff262addc87e2dc484fcba58f94b
radio-satellites <VE3SVF@gmail.com>
2024-11-23 09:09:47 -0500

    msg

--- send_test.py
+++ send_test.py
@@ -20,6 +20,8 @@ payload = bytearray(32) #32 bytes of zeros
 hostname = 'localhost'
 port = 8888
 
+address = "BLLON"
+
 # Connect to pigpiod
 print(f'Connecting to GPIO daemon on {hostname}:{port} ...')
 pi = pigpio.pi(hostname, port)
@@ -28,7 +30,7 @@ if not pi.connected:
     sys.exit()
 
 nrf = NRF24(pi, ce=25, payload_size=RF24_PAYLOAD.DYNAMIC, channel=100, data_rate=RF24_DATA_RATE.RATE_250KBPS, pa_level=RF24_PA.MIN)
-#nrf.set_address_bytes(len(address))
+nrf.set_address_bytes(len(address))
 #nrf.open_writing_pipe(address)
 while True:
     #Capture image
